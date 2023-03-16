085596cca629742333aa223cb1150682368bf1ac
Bastian SOMON <bastian.somon@gmail.com>
2023-03-16 13:47:50 +0000

    Fix script

--- infra/create_registry_secret.sh
+++ infra/create_registry_secret.sh
@@ -1,3 +1,4 @@
+kubectl delete secret registry-creds -n registry-ui
 kubectl create secret generic registry-creds \
     --from-file=.dockerconfigjson=/root/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson \
